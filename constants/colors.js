5c4c0d0b5e487b25b858efddb44a561f7db6442c
shruti-d03 <156565290+shruti-d03@users.noreply.github.com>
2026-03-13 22:51:33 +0530

    Add color constants to colors.js

---
+++ constants/colors.js
@@ -0,0 +1,33 @@
+export const colors = {
+  pink50: '#fff9f7',
+  pink100: '#fff5f5',
+  pink200: '#fce8ee',
+  pink300: '#f4c0d0',
+  pink400: '#ED93B1',
+  pink500: '#D4537E',
+  pink600: '#993556',
+  pink700: '#72243E',
+
+  bgPrimary: '#fff9f7',
+  bgCard: '#ffffff',
+  bgSoft: '#fdf0f5',
+
+  textDark: '#3d1f2d',
+  textMid: '#5d2d40',
+  textMuted: '#c9a0b8',
+  textHint: '#e4a0b8',
+
+  teal: '#1D9E75',
+  tealLight: '#E1F5EE',
+  purple: '#7F77DD',
+  purpleLight: '#EEEDFE',
+  amber: '#BA7517',
+  amberLight: '#FAEEDA',
+  blue: '#378ADD',
+  blueLight: '#E6F1FB',
+  coral: '#D85A30',
+  coralLight: '#FAECE7',
+
+  border: '#fce8ee',
+  borderSoft: '#f0d8d8',
+};
